d92ca741781db0fc9de7bddd715fa5f07d768650
Julio César <julio.apaza@tecsup.edu.pe>
2019-10-30 17:06:09 -0500

    Commit lastcommit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
         $this->call(EvaluatedTableSeeder::class);
         $this->call(EvaluatorTableSeeder::class);
     }
