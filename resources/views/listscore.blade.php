dec4cee9631f67e1721c700e58d72e2fd654d4a3
Julio César <julio.apaza@tecsup.edu.pe>
2019-11-05 14:23:57 -0500

    Commit 05/11 v1.03

--- resources/views/listscore.blade.php
+++ resources/views/listscore.blade.php
@@ -16,18 +16,26 @@
     <table>
       <thead>
         <tr>
-            <th>Name</th>
-            <th>Item Name</th>
-            <th>Item Price</th>
+            <th>Evaluador</th>
+            <th>Evaluado</th>
+            <th>Pregunta1</th>
+            <th>Pregunta2</th>
+            <th>Pregunta3</th>
+            <th>Pregunta4</th>
+            <th>Pregunta5</th>
         </tr>
       </thead>
 
       <tbody>
         @foreach($evaluator->evaluated as $evaluated)
         <tr>
-          <td>{{$evaluator->idEvaluator}}</td>
-          <td></td>
+          <td>{{$evaluator->name}}</td>
+          <td>{{$evaluated->name}}</td>
           <td>{{$evaluated->pivot->answer1}}</td>
+          <td>{{$evaluated->pivot->answer2}}</td>
+          <td>{{$evaluated->pivot->answer3}}</td>
+          <td>{{$evaluated->pivot->answer4}}</td>
+          <td>{{$evaluated->pivot->answer5}}</td>
         </tr>
         @endforeach
       </tbody>
